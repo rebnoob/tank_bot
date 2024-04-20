60a7897436ddacb0d189563fa1bbcb927fea3f1d
rebnoob <ylab2021@mymail.pomona.edu>
2024-04-20 13:50:46 -0700

    .

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -4,6 +4,7 @@ from launch_ros.actions import Node
 import os
 from ament_index_python.packages import get_package_share_directory
 
+
 def generate_launch_description():
 
     joy_params = os.path.join(get_package_share_directory('tank_bot'),'config','joystick.yaml')
@@ -14,6 +15,24 @@ def generate_launch_description():
             parameters=[joy_params],
          )
 
+    teleop_node = Node(
+            package='teleop_twist_joy', 
+            executable='teleop_node',
+            name = 'teleop_node',
+            parameters=[joy_params],
+            remappings=[('/cmd_vel', '/diff_cont/cmd_vel_unstamped')]
+            )
+    # twist_stamper = Node(
+    #         package='twist_stamper',
+    #         executable='twist_stamper',
+    #         parameters=[{'use_sim_time': use_sim_time}],
+    #         remappings=[('/cmd_vel_in','/diff_cont/cmd_vel_unstamped'),
+    #                     ('/cmd_vel_out','/diff_cont/cmd_vel')]
+    #      )
+
+
     return LaunchDescription([
-        joy_node       
+        joy_node,
+        teleop_node,
+        # twist_stamper       
     ])
